1087c98dce00ec5c309372b5a7050e4eee228b2c
Denis Astahov <adv4000@gmail.com>
2023-01-09 19:17:17 -0800

    Matrix x3

--- modules/folder3/main.tf
+++ modules/folder3/main.tf
@@ -1,17 +1,3 @@
-resource "aws_security_group" "web" {
-  name   = "FOLDER-1-WebServer-SG"
-  vpc_id = "vpc-981729797987"
-
-  ingress {
-    description = "Allow port HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
-
-  tags = {
-    Name  = "FOLDER-3-WebServer SG by Terraform"
-    Owner = "Denis Astahov"
-  }
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
