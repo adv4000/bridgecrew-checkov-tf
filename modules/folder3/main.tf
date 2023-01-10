9d559c12d9687545506b0234b8b92685fe8fb471
Denis Astahov <adv4000@gmail.com>
2023-01-09 19:30:19 -0800

    Matrix x4

--- modules/folder3/main.tf
+++ modules/folder3/main.tf
@@ -2,13 +2,3 @@ resource "aws_eip" "lb" {
   instance = aws_instance.ec2.id
   vpc      = true
 }
-
-resource "aws_instance" "ec2" {
-  ami               = "ami-21f78e11"
-  availability_zone = "us-west-2a"
-  instance_type     = "t2.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
